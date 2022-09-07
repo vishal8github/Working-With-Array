2a1a9e38f2d121060cf101460e04f63128c5150f
Vishal Chaurasiya <vishal8study@gmail.com>
2022-09-08 00:31:30 +0530

    merging two arrays using array package

---
+++ AddTwoArraysUsingArrayPkg.py
@@ -0,0 +1,9 @@
+import array
+
+arr1 = array.array('i', [eval(x) for x in input("\nEnter values for array1: ").split()])
+arr2 = array.array('i', [eval(x) for x in input("Enter values for array2: ").split()])
+
+print(f"\nArray1: {arr1}    type: {type(arr1)}")
+print(f"Array2: {arr2}    type: {type(arr2)}")
+
+print(f"\nAddition of two arrays is {arr1 + arr2}")
